7064cc637897bd077174205dbad4f5a7689eb72b
Vero883 <ebullis@ualberta.ca>
2025-03-06 16:26:45 -0700

    emotion unit tests

---
+++ code/app/src/test/java/com/example/moodmasters/EmotionTest.java
@@ -0,0 +1,45 @@
+package com.example.moodmasters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.example.moodmasters.Objects.ObjectsApp.Emotion;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EmotionTest {
+
+    private Emotion emotion;
+
+    @Test
+    public void testGetString() {
+        assertEquals("Happy", Emotion.getString(Emotion.State.HAPPY));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFromStringToEmotionState() {
+        assertEquals(Emotion.State.HAPPY, Emotion.fromStringToEmotionState("Happy"));
+        assertEquals(Emotion.State.HAPPY, Emotion.fromStringToEmotionState("HAPPY"));
+        Emotion.fromStringToEmotionState("abc");
+    }
+
+    @Test
+    public void testGetStringList() {
+        String[] array = {
+                "Happy",
+                "Sad",
+                "Angry",
+                "Scared",
+                "Disgusted",
+                "Confused",
+                "Ashamed",
+                "Surprised"
+        };
+        List<String> emotion_list = Emotion.getStringList();
+        assertTrue(emotion_list.containsAll(Arrays.asList(array)));
+    }
+
+}
