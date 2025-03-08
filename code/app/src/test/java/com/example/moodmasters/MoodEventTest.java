68957bcceef675c37c453234d3f241234283b5ca
Vero883 <ebullis@ualberta.ca>
2025-03-07 18:40:53 -0700

    MoodEvent tests

--- code/app/src/test/java/com/example/moodmasters/MoodEventTest.java
+++ code/app/src/test/java/com/example/moodmasters/MoodEventTest.java
@@ -6,15 +6,22 @@ import com.example.moodmasters.Objects.ObjectsApp.MoodEvent;
 import com.example.moodmasters.Objects.ObjectsApp.SocialSituation;
 
 import org.junit.Before;
+import org.junit.Test;
 
 public class MoodEventTest {
 
     MoodEvent mood_event;
+    Mood mood;
+    Long epoch_time;
 
     @Before
     public void setUp() {
-        long epoch_time = Long.parseLong("1741155652020");
-        Mood mood = new Mood(Emotion.State.HAPPY, R.color.mood_happy_color, R.string.mood_emoji_happy);
+        epoch_time = Long.parseLong("1741155652020");
+        mood = new Mood(Emotion.State.HAPPY, R.color.mood_happy_color, R.string.mood_emoji_happy);
+    }
+
+    @Test
+    public void testConstructor() {
         mood_event = new MoodEvent(
                 "Mar 01 2025 | 12:00",
                 epoch_time,
@@ -22,6 +29,30 @@ public class MoodEventTest {
                 "reason",
                 "trigger",
                 SocialSituation.State.ALONE
+        );
+        mood_event = new MoodEvent(
+                "Mar 01 2025 | 12:00",
+                epoch_time,
+                mood,
+                null,
+                "trigger",
+                SocialSituation.State.ALONE
+        );
+        mood_event = new MoodEvent(
+                "Mar 01 2025 | 12:00",
+                epoch_time,
+                mood,
+                "reason",
+                null,
+                SocialSituation.State.ALONE
+        );
+        mood_event = new MoodEvent(
+                "Mar 01 2025 | 12:00",
+                epoch_time,
+                mood,
+                "reason",
+                "trigger",
+                null
         );
     }
 
