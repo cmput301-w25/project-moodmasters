19922744e5c3c3065c2ffbd6d39b14a47fe9a51c
Vero883 <ebullis@ualberta.ca>
2025-03-08 15:48:11 -0700

    started add mood tests

--- code/app/src/androidTest/java/com/example/moodmasters/AddMoodTest.java
+++ code/app/src/androidTest/java/com/example/moodmasters/AddMoodTest.java
@@ -1,5 +1,14 @@
 package com.example.moodmasters;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.util.Log;
+
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -10,10 +19,18 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -39,8 +56,61 @@ public class AddMoodTest {
         };
 
         for (Participant participant : participants) {
-            DocumentReference doc_ref = participants_ref.document();
+            DocumentReference doc_ref = participants_ref.document(participant.getUsername());
             doc_ref.set(participant);
+        }
+    }
+
+    @Before
+    public void navigateToMainScreen() {
+        onView(withId(R.id.signup_login_enter_username)).perform(ViewActions.typeText("user_2"));
+        onView(withId(R.id.signup_login_ok_button)).perform(ViewActions.click());
+    }
+
+    @Test
+    public void testAddMood() {
+        onView(withId(R.id.user_mood_history_add_button)).perform(ViewActions.click());
+        onView(withId(R.id.alter_mood_emotion_spinner)).perform(ViewActions.click());
+        onView(withText("Sad")).perform(ViewActions.click());
+        onView(withId(R.id.alter_mood_ok_button)).perform(ViewActions.click());
+        onView(withText("Sad")).check(matches(isDisplayed()));
+
+        onView(withId(R.id.user_mood_history_add_button)).perform(ViewActions.click());
+        onView(withId(R.id.alter_mood_emotion_spinner)).perform(ViewActions.click());
+        onView(withText("Happy")).perform(ViewActions.click());
+        onView(withId(R.id.alter_mood_ok_button)).perform(ViewActions.click());
+        onView(withText("Happy")).check(matches(isDisplayed()));
+        onView(withText("Sad")).check(matches(isDisplayed()));
+
+        onView(withId(R.id.user_mood_history_menu_button)).perform(ViewActions.click());
+        onView(withId(R.id.options_logout_button)).perform(ViewActions.click());
+        onView(withId(R.id.signup_login_enter_username)).perform(ViewActions.typeText("user_2"));
+        onView(withId(R.id.signup_login_ok_button)).perform(ViewActions.click());
+        onView(withText("Happy")).check(matches(isDisplayed()));
+        onView(withText("Sad")).check(matches(isDisplayed()));
+    }
+
+    @After
+    public void tearDown() {
+        String projectId = "moodmastersapp";
+        URL url = null;
+        try {
+            url = new URL("http://10.0.2.2:8080/emulator/v1/projects/" + projectId + "/databases/(default)/documents");
+        } catch (MalformedURLException exception) {
+            Log.e("URL Error", Objects.requireNonNull(exception.getMessage()));
+        }
+        HttpURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("DELETE");
+            int response = urlConnection.getResponseCode();
+            Log.i("Response Code", "Response Code: " + response);
+        } catch (IOException exception) {
+            Log.e("IO Error", Objects.requireNonNull(exception.getMessage()));
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
         }
     }
 }
