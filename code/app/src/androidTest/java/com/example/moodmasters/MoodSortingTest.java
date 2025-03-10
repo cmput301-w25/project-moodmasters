464a46f78aa8dc5361c186c55b566b12cd19703d
Vero883 <ebullis@ualberta.ca>
2025-03-10 15:58:06 -0600

    testing fixes

--- code/app/src/androidTest/java/com/example/moodmasters/MoodSortingTest.java
+++ code/app/src/androidTest/java/com/example/moodmasters/MoodSortingTest.java
@@ -4,33 +4,34 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import android.view.View;
+import android.util.Log;
 
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
-import com.example.moodmasters.Objects.ObjectsApp.Emotion;
-import com.example.moodmasters.Objects.ObjectsApp.MoodEvent;
-import com.example.moodmasters.Objects.ObjectsApp.SocialSituation;
-import com.example.moodmasters.Objects.ObjectsBackend.MoodList;
+import com.example.moodmasters.Views.SignupLoginScreenActivity;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-import org.hamcrest.Matcher;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import com.example.moodmasters.Objects.ObjectsApp.Mood;
-import com.example.moodmasters.Views.SignupLoginScreenActivity;
-import com.google.firebase.firestore.FirebaseFirestore;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
 
-
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MoodSortingTest {
     // Tests that Mood list Sorting is sorted by date and time, in reverse chronological order (most recent coming first)
 
@@ -39,7 +40,7 @@ public class MoodSortingTest {
             ActivityScenarioRule<SignupLoginScreenActivity>(SignupLoginScreenActivity.class);
 
     @BeforeClass
-    public static void setup(){
+    public static void setup() {
         // Specific address for emulated device to access our localHost
         String androidLocalhost = "10.0.2.2";
         int portNumber = 8080;
@@ -48,13 +49,13 @@ public class MoodSortingTest {
 
 
     @Before
-        public void navigateToMainScreen() {
-            onView(withId(R.id.signup_login_enter_username)).perform(ViewActions.typeText("user_3"));
-            onView(withId(R.id.signup_login_ok_button)).perform(ViewActions.click());
+    public void navigateToMainScreen() {
+        onView(withId(R.id.signup_login_enter_username)).perform(ViewActions.typeText("user_3"));
+        onView(withId(R.id.signup_login_ok_button)).perform(ViewActions.click());
     }
 
     @Test
-    public void testMoodsareSorted() {
+    public void testMoodsAreSorted() {
         long sadTime = System.currentTimeMillis();
         onView(withId(R.id.user_mood_history_add_button)).perform(ViewActions.click());
         onView(withId(R.id.alter_mood_emotion_spinner)).perform(ViewActions.click());
@@ -82,11 +83,29 @@ public class MoodSortingTest {
         // Check that the first item in the list is sad
         onView(withText("Sad")).check(matches(isDisplayed()));
         onView(withText("Happy")).check(matches(isDisplayed()));
-        }
-
-
     }
 
-
-
-
+    @After
+    public void tearDown() {
+        String projectId = "moodmastersapp";
+        URL url = null;
+        try {
+            url = new URL("http://10.0.2.2:8080/emulator/v1/projects/" + projectId + "/databases/(default)/documents");
+        } catch (MalformedURLException exception) {
+            Log.e("URL Error", Objects.requireNonNull(exception.getMessage()));
+        }
+        HttpURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("DELETE");
+            int response = urlConnection.getResponseCode();
+            Log.i("Response Code", "Response Code: " + response);
+        } catch (IOException exception) {
+            Log.e("IO Error", Objects.requireNonNull(exception.getMessage()));
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+    }
+}
