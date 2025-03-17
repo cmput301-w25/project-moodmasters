f48e8b18a7231edb087e9e8a2ffa39a25beb93ce
Vero883 <ebullis@ualberta.ca>
2025-03-16 20:33:56 -0600

    trying to get map to work

--- code/app/src/main/java/com/example/moodmasters/Views/MoodEventsMapActivity.java
+++ code/app/src/main/java/com/example/moodmasters/Views/MoodEventsMapActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.moodmasters.Events.MoodEventsMapBackEvent;
@@ -12,8 +13,13 @@ import com.example.moodmasters.MVC.MVCView;
 import com.example.moodmasters.Objects.ObjectsMisc.MoodEventList;
 import com.example.moodmasters.Objects.ObjectsMisc.MoodMap;
 import com.example.moodmasters.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MoodEventsMapActivity extends AppCompatActivity implements MVCView {
+public class MoodEventsMapActivity extends AppCompatActivity implements OnMapReadyCallback, MVCView {
     private MoodEventList mood_events;
     private MoodMap mood_map;
 
@@ -33,9 +39,21 @@ public class MoodEventsMapActivity extends AppCompatActivity implements MVCView 
         EdgeToEdge.enable(this);
         setContentView(R.layout.mood_events_map_screen);
 
+        MapView map_view = findViewById(R.id.map_screen_map_view);
+        map_view.onCreate(savedInstanceState);
+
+        map_view.getMapAsync(this);
+
         Button back_button = findViewById(R.id.map_screen_back_button);
         back_button.setOnClickListener(v -> {
             controller.execute(new MoodEventsMapBackEvent(), this);
         });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
     }
 }
