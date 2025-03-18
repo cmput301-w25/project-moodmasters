ff7e85de49896df4a6c7b54dc95e7c3a823c4308
Kaddz73 <153503633+Kaddz73@users.noreply.github.com>
2025-03-18 05:30:39 -0600

    Implementing follow requests, accepting follow requests, following

--- code/app/src/main/java/com/example/moodmasters/Objects/ObjectsBackend/FollowingList.java
+++ code/app/src/main/java/com/example/moodmasters/Objects/ObjectsBackend/FollowingList.java
@@ -1,28 +1,67 @@
 package com.example.moodmasters.Objects.ObjectsBackend;
 
-import com.example.moodmasters.MVC.MVCBackendList;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.CollectionReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
-// Not needed for halfway checkpoint
+public class FollowingList {
+    private FirebaseFirestore db;
+    private String userId;
+    private CollectionReference followingRef;
+    private CollectionReference followRequestsRef;
 
-//public class FollowingList extends MVCBackendList<Participant> {
-//    private MoodFollowingList mood_following_list;          /*This will also be accessible from the model for easy access*/
-//    public FollowingList(){
-//        super();
-//    }
-//    public void setDatabaseData(DocumentReference docRef, DocumentSnapshot snapshot){
-//        // TODO: Implement getting and possibly setting initial data from database
-//    }
-//    public void updateDatabaseData(DocumentReference docRef){
-//        // TODO: Implement updating database with added/removed data
-//    }
-//
-//    // called when model.createBackendObject(MOODFOLLOWINGLIST) is called
-//    public MoodFollowingList createFollowingList(){
-//        mood_following_list = new MoodFollowingList(FirebaseFirestore.getInstance());
-//        // TODO: Create Mood Following List From participants
-//        return mood_following_list;
-//    }
-//}
+    public FollowingList(String userId) {
+        this.db = FirebaseFirestore.getInstance();
+        this.userId = userId;
+        this.followingRef = db.collection("participants").document(userId).collection("following");
+        this.followRequestsRef = db.collection("participants").document(userId).collection("followRequests");
+    }
+
+    // Fetch list of following users
+    public void fetchFollowing(Consumer<List<String>> callback) {
+        followingRef.get().addOnCompleteListener(task -> {
+            List<String> following = new ArrayList<>();
+            if (task.isSuccessful()) {
+                for (var document : task.getResult()) {
+                    following.add(document.getId());
+                }
+            }
+            callback.accept(following);
+        });
+    }
+
+    // Send follow request
+    public void sendFollowRequest(String targetUserId) {
+        DocumentReference targetRef = db.collection("participants").document(targetUserId)
+                .collection("followRequests").document(userId);
+        targetRef.set(new Object()); // Add requester to follow requests
+    }
+
+    // Accept follow request
+    public void acceptFollowRequest(String requesterId) {
+        DocumentReference requesterRef = followingRef.document(requesterId);
+        requesterRef.set(new Object()); // Move to following list
+        followRequestsRef.document(requesterId).delete(); // Remove from requests
+    }
+
+    // Reject follow request
+    public void rejectFollowRequest(String requesterId) {
+        followRequestsRef.document(requesterId).delete();
+    }
+
+    // Fetch follow requests
+    public void fetchFollowRequests(Consumer<List<String>> callback) {
+        followRequestsRef.get().addOnCompleteListener(task -> {
+            List<String> requesters = new ArrayList<>();
+            if (task.isSuccessful()) {
+                for (var document : task.getResult()) {
+                    requesters.add(document.getId());
+                }
+            }
+            callback.accept(requesters);
+        });
+    }
+}
