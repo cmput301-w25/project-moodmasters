25cbcd037ef8db9f37c8f658128f47d22219a150
Vero883 <ebullis@ualberta.ca>
2025-03-18 19:48:13 -0600

    Revert "Reapply "Merge pull request #145 from cmput301-w25/main""
    This reverts commit a04e8a8f1f7543346dada74fe1a2a79e10096f11.

--- code/app/src/androidTest/java/com/example/moodmasters/EditMoodTest.java
+++ code/app/src/androidTest/java/com/example/moodmasters/EditMoodTest.java
@@ -1,7 +1,6 @@
 package com.example.moodmasters;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -43,71 +42,49 @@ public class EditMoodTest {
         // Specific address for emulated device to access our localHost
         String androidLocalhost = "10.0.2.2";
         int portNumber = 8080;
-        try{
-            FirebaseFirestore.getInstance().useEmulator(androidLocalhost, portNumber);
-        }
-        catch (Exception ignore){
-
-        }
+        FirebaseFirestore.getInstance().useEmulator(androidLocalhost, portNumber);
     }
 
     @Before
     public void navigateToMainScreen() {
-        onView(withId(R.id.signup_login_enter_username)).perform(ViewActions.typeText("user_4"));
-        onView(withId(R.id.signup_login_enter_username)).perform(closeSoftKeyboard());
+        onView(withId(R.id.signup_login_enter_username)).perform(ViewActions.typeText("user_1"));
         onView(withId(R.id.signup_login_ok_button)).perform(ViewActions.click());
     }
 
     @Test
-    public void testEditMood() throws Exception{
-        Thread.sleep(1000);
+    public void testEditMood() {
         // 1 mood
         onView(withId(R.id.user_mood_history_add_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
         onView(withId(R.id.alter_mood_emotion_spinner)).perform(ViewActions.click());
         onView(withText("Sad")).perform(ViewActions.click());
         onView(withId(R.id.alter_mood_ok_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
-
         onView(withText("Sad")).check(matches(isDisplayed()));
 
         // 2 moods
         onView(withId(R.id.user_mood_history_add_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
         onView(withId(R.id.alter_mood_emotion_spinner)).perform(ViewActions.click());
         onView(withText("Happy")).perform(ViewActions.click());
         onView(withId(R.id.alter_mood_ok_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
         onView(withText("Happy")).check(matches(isDisplayed()));
         onView(withText("Sad")).check(matches(isDisplayed()));
 
         // Edit one mood
         onView(withText("Sad")).perform(ViewActions.longClick());
-        Thread.sleep(1000);
         onView(withId(R.id.view_mood_edit_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
         onView(withId(R.id.alter_mood_emotion_spinner)).perform(ViewActions.click());
         onView(withText("Angry")).perform(ViewActions.click());
         onView(withId(R.id.alter_mood_ok_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
         onView(withId(R.id.view_mood_x_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
         onView(withText("Sad")).check(doesNotExist());
         onView(withText("Angry")).check(matches(isDisplayed()));
         onView(withText("Happy")).check(matches(isDisplayed()));
 
         // Logout/login
         onView(withId(R.id.user_mood_history_menu_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
         onView(withId(R.id.options_logout_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
         onView(withId(R.id.signup_login_change_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
-        onView(withId(R.id.signup_login_enter_username)).perform(ViewActions.typeText("user_4"));
-        onView(withId(R.id.signup_login_enter_username)).perform(closeSoftKeyboard());
-        Thread.sleep(1000);
+        onView(withId(R.id.signup_login_enter_username)).perform(ViewActions.typeText("user_1"));
         onView(withId(R.id.signup_login_ok_button)).perform(ViewActions.click());
-        Thread.sleep(1000);
         onView(withText("Sad")).check(doesNotExist());
         onView(withText("Angry")).check(matches(isDisplayed()));
         onView(withText("Happy")).check(matches(isDisplayed()));
