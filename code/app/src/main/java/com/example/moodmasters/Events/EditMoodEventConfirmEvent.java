5acf6e273af69f64ed5aa469adfdf8a69e14e085
Vero883 <ebullis@ualberta.ca>
2025-03-21 09:05:43 -0600

    character limit updated

--- code/app/src/main/java/com/example/moodmasters/Events/EditMoodEventConfirmEvent.java
+++ code/app/src/main/java/com/example/moodmasters/Events/EditMoodEventConfirmEvent.java
@@ -48,7 +48,7 @@ public class EditMoodEventConfirmEvent implements MVCController.MVCEvent {
 
         Participant user = ((Participant) model.getBackendObject(BackendObject.State.USER));
 
-        if (!activity.addDataVerification(reason_string)){
+        if (activity.addDataVerification(reason_string)){
             reason_text.setError("Must be less than 20 characters and only 3 words");
             return;
         }
