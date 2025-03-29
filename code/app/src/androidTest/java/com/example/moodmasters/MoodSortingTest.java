5f3598f43f489dc13410a1783fe41239caba596b
Vero883 <ebullis@ualberta.ca>
2025-03-29 13:13:27 -0600

    started sorting test

--- code/app/src/androidTest/java/com/example/moodmasters/MoodSortingTest.java
+++ code/app/src/androidTest/java/com/example/moodmasters/MoodSortingTest.java
@@ -56,8 +56,12 @@ public class MoodSortingTest {
 
     @Before
     public void navigateToMainScreen() {
+        onView(withId(R.id.signup_login_change_button)).perform(ViewActions.click());
+        //Thread.sleep(1000);
         onView(withId(R.id.signup_login_enter_username)).perform(ViewActions.typeText("user_7"));
         onView(withId(R.id.signup_login_enter_username)).perform(closeSoftKeyboard());
+        onView(withId(R.id.signup_login_enter_password)).perform(ViewActions.typeText("user_7"));
+        onView(withId(R.id.signup_login_enter_password)).perform(closeSoftKeyboard());
         onView(withId(R.id.signup_login_ok_button)).perform(ViewActions.click());
     }
     /**
